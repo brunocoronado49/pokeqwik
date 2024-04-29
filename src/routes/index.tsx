f26686849bce770fa370c25373c090cb7e3dd36d
Francisco Rangel <francorangelcoronado@gmail.com>
2024-04-29 07:00:48 -0600

    Agregando signals para cambiar el estado del componente y consumiendo la pokeapi para mostrar imagenes de pokemones con dos botones para cambiar el id

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,20 +1,37 @@
-import { component$ } from "@builder.io/qwik";
+import { component$, useSignal, $ } from "@builder.io/qwik";
 import type { DocumentHead } from "@builder.io/qwik-city";
 
 export default component$(() => {
+  const pokemonId = useSignal<number>(1); // use for primitives types
+
+  const changePokemonId = $((value: number) => {
+    if ((pokemonId.value + value) <= 0) return;
+    pokemonId.value += value;
+  });
+
   return (
     <>
-      <span class="text-5xl">Hola Mundo</span>
+      <span class="text-2xl">Buscador simple</span>
+      <span class="text-9xl">{ pokemonId }</span>
+
+      <img width="250" height="250"
+        src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${pokemonId.value}.png`}
+        alt="pokemon" />
+
+      <div class="mt-2">
+        <button onClick$={ () => changePokemonId(-1) } class="btn btn-primary mr-2">Previous</button>
+        <button onClick$={ () => changePokemonId(+1) } class="btn btn-primary">Next</button>
+      </div>
     </>
   );
 });
 
 export const head: DocumentHead = {
-  title: "HDTPM Qwik",
+  title: "Hello PokeQwik",
   meta: [
     {
       name: "description",
-      content: "Qwik site description",
+      content: "This is my first Qwik app",
     },
   ],
 };
